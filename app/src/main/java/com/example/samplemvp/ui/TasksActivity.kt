b9b8a2aba4cfd8bf7db1093d20d329d3d751e9d1
Parkduksung <duksung1234@naver.com>
2021-09-28 21:40:09 +0900

    [구글-MVP][feature]navigationView 적용. isChecked 는 false 를 준 이유는 다시 navigationView 를 봤을때 선택되어져 있는거로 보이기때문이다. 그리고 closeDrawers 를 주는 이유는 선택시 drawerLayout 이 닫혀지게 하기 위해서이다. 맨 마지막에 true, false 는 내부 소스를 확인해보니 선택되었을때 알리냐 안알리냐 차이인거 같은데.. 어디서 확인해야 하는지 모르겠다..ㅠ

--- app/src/main/java/com/example/samplemvp/ui/TasksActivity.kt
+++ app/src/main/java/com/example/samplemvp/ui/TasksActivity.kt
@@ -1,12 +1,20 @@
 package com.example.samplemvp.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.samplemvp.R
 import com.example.samplemvp.util.setupActionBar
+import com.google.android.material.navigation.NavigationView
 
 class TasksActivity : AppCompatActivity() {
 
+    private lateinit var drawerLayout: DrawerLayout
+
+    private val isClick by lazy {  findViewById<NavigationView>(R.id.nav_view).isSelected }
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tasks_act)
@@ -16,5 +24,29 @@ class TasksActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
         }
 
+        drawerLayout = (findViewById<DrawerLayout>(R.id.drawer_layout)).apply {
+            setStatusBarBackground(R.color.colorPrimaryDark)
+        }
+
+        setupDrawerContent(findViewById(R.id.nav_view))
+
+
+    }
+
+    private fun setupDrawerContent(navigationView: NavigationView) {
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.statistics_navigation_menu_item -> {
+                    Toast.makeText(this, "click statistics", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.list_navigation_menu_item -> {
+                    Toast.makeText(this, "click to-do list", Toast.LENGTH_SHORT).show()
+                }
+            }
+            menuItem.isChecked = false
+            drawerLayout.closeDrawers()
+            true
+        }
     }
 }
