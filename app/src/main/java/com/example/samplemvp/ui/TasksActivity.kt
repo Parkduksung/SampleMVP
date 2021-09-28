4affba1679b8311d2674f6a4aa4f9343992125f4
Parkduksung <duksung1234@naver.com>
2021-09-28 21:09:21 +0900

    [구글-MVP][feature]toolbar 적용.

--- app/src/main/java/com/example/samplemvp/ui/TasksActivity.kt
+++ app/src/main/java/com/example/samplemvp/ui/TasksActivity.kt
@@ -1,4 +1,20 @@
 package com.example.samplemvp.ui
 
-class TasksActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.samplemvp.R
+import com.example.samplemvp.util.setupActionBar
+
+class TasksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tasks_act)
+
+        setupActionBar(R.id.toolbar) {
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+            setDisplayHomeAsUpEnabled(true)
+        }
+
+    }
 }
