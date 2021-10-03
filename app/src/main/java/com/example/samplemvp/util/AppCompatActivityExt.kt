e11957514e6ac6ce327a7ae3096a16d9593f78b0
Parkduksung <duksung1234@naver.com>
2021-10-03 23:14:23 +0900

    [구글-MVP][feature] replaceFragmentInActivity 추가.

--- app/src/main/java/com/example/samplemvp/util/AppCompatActivityExt.kt
+++ app/src/main/java/com/example/samplemvp/util/AppCompatActivityExt.kt
@@ -3,10 +3,15 @@ package com.example.samplemvp.util
 import androidx.annotation.IdRes
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
-fun AppCompatActivity.setupActionBar(@IdRes toolbarId : Int, action : ActionBar.() -> Unit){
+fun AppCompatActivity.setupActionBar(@IdRes toolbarId: Int, action: ActionBar.() -> Unit) {
     setSupportActionBar(findViewById(toolbarId))
     supportActionBar?.run {
         action()
     }
+}
+
+fun AppCompatActivity.replaceFragmentInActivity(fragment: Fragment, @IdRes frameId: Int) {
+    supportFragmentManager.beginTransaction().replace(frameId, fragment).commit()
 }
