7963d953ab40b126300a9b7668c8d632c514170d
Parkduksung <duksung1234@naver.com>
2021-10-04 17:11:07 +0900

    [구글-MVP][feature] AppCompatActivityExt 수정.

--- app/src/main/java/com/example/samplemvp/util/AppCompatActivityExt.kt
+++ app/src/main/java/com/example/samplemvp/util/AppCompatActivityExt.kt
@@ -4,6 +4,8 @@ import androidx.annotation.IdRes
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 fun AppCompatActivity.setupActionBar(@IdRes toolbarId: Int, action: ActionBar.() -> Unit) {
     setSupportActionBar(findViewById(toolbarId))
@@ -12,6 +14,28 @@ fun AppCompatActivity.setupActionBar(@IdRes toolbarId: Int, action: ActionBar.()
     }
 }
 
+/**
+ * The `fragment` is added to the container view with tag. The operation is
+ * performed by the `fragmentManager`.
+ */
+fun AppCompatActivity.addFragmentToActivity(fragment: Fragment, tag: String) {
+    supportFragmentManager.transact {
+        add(fragment, tag)
+    }
+}
+
 fun AppCompatActivity.replaceFragmentInActivity(fragment: Fragment, @IdRes frameId: Int) {
-    supportFragmentManager.beginTransaction().replace(frameId, fragment).commit()
+    supportFragmentManager.transact {
+        replace(frameId, fragment).commit()
+    }
+}
+
+
+/**
+ * Runs a FragmentTransaction, then calls commit().
+ */
+private inline fun FragmentManager.transact(action: FragmentTransaction.() -> Unit) {
+    beginTransaction().apply {
+        action()
+    }.commit()
 }
