c5913247f28322dc8ab4f0ad264e4743f355a215
Parkduksung <duksung1234@naver.com>
2021-10-04 16:54:50 +0900

    [구글-MVP][feature] TasksDao insert 작업.

--- app/src/main/java/com/example/samplemvp/data/source/local/TasksDao.kt
+++ app/src/main/java/com/example/samplemvp/data/source/local/TasksDao.kt
@@ -1,4 +1,21 @@
 package com.example.samplemvp.data.source.local
 
-interface TasksDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.example.android.architecture.blueprints.todoapp.data.Task
+
+/**
+ * Task 테이블의 데이터 액세스 개체
+ */
+@Dao interface TasksDao {
+
+    /**
+     * 데이터베이스 안에 Task 를 추가한다.
+     *
+     * 만일 Task 가 이미 존재하면 replace 한다.
+     *
+     * @param task 추가되는 Task
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE) fun insertTask(task : Task)
 }
